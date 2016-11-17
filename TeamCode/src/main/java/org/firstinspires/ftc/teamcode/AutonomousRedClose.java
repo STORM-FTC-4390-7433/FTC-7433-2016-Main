aedfc37f4dc98708aa3632655daf92aa48f9f419
gregoryhazen <gregoryhazen@gmail.com>
2016-11-17 17:39:01 -0500

    Error

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRedClose.java
@@ -0,0 +1,294 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.ThreadPool;
+import com.vuforia.HINT;
+import com.vuforia.Vuforia;
+import com.vuforia.ar.pl.SystemTools;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import java.security.Timestamp;
+
+/**
+ * Created by grego on 10/18/2016.
+ */
+@Autonomous(name="RedClose", group="Vision")
+public class AutonomousRedClose extends LinearOpMode {
+    //OpenGLMatrix pose = new OpenGLMatrix();
+    private DcMotor leftMotor = null, rightMotor = null;
+    private Servo beaconServo = null;
+    private ColorSensor colorSensor;
+    private DeviceInterfaceModule CDI;
+    private float hsvValues[] = {0, 0, 0};
+    private int distanceAdjust = -1000;
+    private boolean adjust = false;
+    private String color = "blue";
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1368 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 3.8 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+       /*
+        * Initialize the drive system variables.
+        * The init() method of the hardware class does all the work here
+        */
+
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.update();
+
+
+        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        idle();
+
+
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Path0", "Starting at %7d :%7d",
+                leftMotor.getCurrentPosition(),
+                rightMotor.getCurrentPosition());
+        telemetry.update();
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+
+        // Step through each leg of the path,
+        // Note: Reverse movement is obtained by setting a negative distance (not speed)
+        encoderDrive(DRIVE_SPEED, 25, 25, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(TURN_SPEED, 14.8, -14.8, 1.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED, 12, 12, 1.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+
+
+        VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
+        params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        params.vuforiaLicenseKey = "ATL2vJ3/////AAAAGZZx51v2h0D2kh6vX9dkEVwwXavfMtPW74LnE7NWXWw2NChN8Td99tPKhECwV61l/fTsgxV43ktU6XBUlR9lZn1Z3BEd7nQPD+s4uscCWDSjTpXDdQZZWVD7Cfmp+ZK8ax49W55s1vC6mX3vED8miPeegc8DR1bT2BtjxLa0cD77nbeVN5ztUzZEGKPTZEhxGoxjqQsKOEUktyLo6NZIRTA5uEhOmVuwVWC1Iq49tfbjKnLe7t1qfzQlB6wri9DPUrtt3YeuyrNERLclghW7fz7GrfWooMfQIaNEbu/E7BhY95CDGy/Srl1ZpvingaBdcfpB7MAQ/+bFw93saY/lYwT7MXu9ctO9zv1rmuFEAJFp";
+        params.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES;
+
+        VuforiaLocalizer vuforia = ClassFactory.createVuforiaLocalizer(params);
+        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
+
+        VuforiaTrackables beacons = vuforia.loadTrackablesFromAsset("FTC_2016-17");
+        beacons.get(0).setName("Wheels");
+        beacons.get(1).setName("Tools");
+        beacons.get(2).setName("Lego");
+        beacons.get(3).setName("Gears");
+
+        leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+        beaconServo = hardwareMap.servo.get("beaconServo");
+        colorSensor = hardwareMap.colorSensor.get("color");
+        CDI = hardwareMap.deviceInterfaceModule.get("Device Interface Module 1");
+        colorSensor.enableLed(false);
+
+
+        waitForStart();
+
+        beacons.activate();
+        int noSee = 0;
+        int see = 0;
+        long lastTime = 0;
+        boolean run = false;
+        int state = 0;
+        while (opModeIsActive()) {
+            Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
+            telemetry.addData("2 Clear", colorSensor.alpha());
+            telemetry.addData("3 Red  ", colorSensor.red());
+            telemetry.addData("4 Green", colorSensor.green());
+            telemetry.addData("5 Blue ", colorSensor.blue());
+            telemetry.addData("6 Hue  ", hsvValues[0]);
+
+
+            for (VuforiaTrackable beac : beacons) {
+                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) beac.getListener()).getPose();
+
+                if (pose != null) {
+
+                    VectorF translation = pose.getTranslation();
+
+                    telemetry.addData(beac.getName() + "-Translation", translation);
+
+                    double degreesToTurn = (180 - Math.toDegrees(Math.atan2(translation.get(0), translation.get(2))));
+
+                    telemetry.addData(beac.getName() + "-Degrees", degreesToTurn);
+
+                    telemetry.update();
+
+
+                    if (state == 0) {
+                        if (degreesToTurn > 2 && degreesToTurn < 180) {
+
+                            leftMotor.setPower(-.15);
+                            rightMotor.setPower(.15);
+                            adjust = false;
+
+
+                        } else if (degreesToTurn < 358 && degreesToTurn > 180) {
+
+                            leftMotor.setPower(.15);
+                            rightMotor.setPower(-.15);
+                            adjust = false;
+
+                        } else if ((degreesToTurn >= 358 || degreesToTurn <= 2)) {
+
+                            leftMotor.setPower(0);
+                            rightMotor.setPower(0);
+                            adjust = true;
+                            state = 1;
+                        }
+                    }
+                    if (state == 1) {
+
+                        if (translation.get(2) < distanceAdjust && translation.get(2) < -200) {
+                            rightMotor.setPower(.75);
+                            leftMotor.setPower(.75);
+                        } else if (translation.get(2) > -200 && adjust) {
+                            state = 2;
+                        } else {
+                            rightMotor.setPower(0);
+                            leftMotor.setPower(0);
+                            distanceAdjust /= 2;
+                            state = 0;
+                            adjust = false;
+                        }
+                    }
+
+                    if (state == 2) {
+                        rightMotor.setPower(.25);
+                        leftMotor.setPower(.25);
+                        Thread.sleep(850);
+                        rightMotor.setPower(0);
+                        leftMotor.setPower(0);
+                        state = 3;
+                    }
+
+                    if (state == 3) {
+                        rightMotor.setPower(-.5);
+                        leftMotor.setPower(-.5);
+                        Thread.sleep(300);
+                        rightMotor.setPower(0);
+                        leftMotor.setPower(0);
+                        if (colorSensor.red() > colorSensor.blue() && color == "blue") {
+                            beaconServo.setPosition(.8);
+                        } else if (colorSensor.blue() > colorSensor.red() && color == "blue") {
+                            beaconServo.setPosition(.1);
+                        }
+                        state = 4;
+                    }
+
+                    if (state == 4) {
+                        leftMotor.setPower(.75);
+                        rightMotor.setPower(.75);
+                        Thread.sleep(800);
+                        leftMotor.setPower(0);
+                        rightMotor.setPower(0);
+                        state = 5;
+                    }
+
+                }
+            }
+            telemetry.update();
+        }
+    }
+
+
+    public void encoderDrive(double speed,
+        double leftInches, double rightInches,
+        double timeoutS) throws InterruptedException {
+            int newLeftTarget;
+            int newRightTarget;
+
+
+            // Ensure that the opmode is still active
+            if (opModeIsActive()) {
+
+
+                // Determine new target position, and pass to motor controller
+                newLeftTarget = leftMotor.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+                newRightTarget = rightMotor.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+                leftMotor.setTargetPosition(newLeftTarget);
+                rightMotor.setTargetPosition(newRightTarget);
+
+
+                // Turn On RUN_TO_POSITION
+                leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+                // reset the timeout time and start motion.
+                runtime.reset();
+                leftMotor.setPower(Math.abs(speed));
+                rightMotor.setPower(Math.abs(speed));
+
+
+                // keep looping while we are still active, and there is time left, and both motors are running.
+                while (opModeIsActive() &&
+                        (runtime.seconds() < timeoutS) &&
+                        (leftMotor.isBusy() && rightMotor.isBusy())) {
+
+
+                    // Display it for the driver.
+                    telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+                    telemetry.addData("Path2",  "Running at %7d :%7d",
+                            leftMotor.getCurrentPosition(),
+                            rightMotor.getCurrentPosition());
+                    telemetry.update();
+
+
+                    // Allow time for other processes to run.
+                    idle();
+                }
+
+
+                // Stop all motion;
+                leftMotor.setPower(0);
+                rightMotor.setPower(0);
+
+
+                // Turn off RUN_TO_POSITION
+                leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+                //  sleep(250);   // optional pause after each move
+            }
+        }
+    }
